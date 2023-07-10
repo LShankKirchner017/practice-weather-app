ad4726cd8ea9159ef51f557c65b17c678d785272
Lindsay <lshank017@gmail.com>
2023-07-10 07:21:05 -0500

    set up API and fetch for weather

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,14 @@
 let weather = {
-  apiKey: "6ad17921c1cde5846951cca12f7bb7b5",
-};
+  "apiKey": "6ad17921c1cde5846951cca12f7bb7b5",
+  fetchWeather: function(city){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+    + city 
+    + "&units=imperial&appid="
+    + this.apiKey
+    )
+    .then((response) => response.json()).then((data) => console.log(data));
+  },
+  displayWeather: function(data){
+
+  }
+  }
